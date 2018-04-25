384249e0b9ddc099d90eec82e953e6cd1dcee2ad
ronetrabalhos <rone.trabalhos@gmail.com>
2018-04-25 14:33:23 -0300

    Parallax
    Adaptações no código anterior e estudo parallax

---
+++ parallax/01/javascript.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $('div.bgParallax').each(function(){
+        var $obj = $(this);
+    
+        $(window).scroll(function() {
+            var yPos = -($(window).scrollTop() / $obj.data('speed')); 
+    
+            var bgpos = '50% '+ yPos + 'px';
+            // console.log('- ' + bgpos);
+            
+            $obj.css('background-position', bgpos );
+    
+        }); 
+    });	
+});
+        
